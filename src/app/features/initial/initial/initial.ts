1f009b892ef9c0a768757e200e3e8053c9d0e500
Clara <clararemotto@gmail.com>
2025-07-02 17:44:30 -0300

    feat: add mobile breakpoint

--- src/app/features/initial/initial/initial.ts
+++ src/app/features/initial/initial/initial.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { Navbar } from "../../../shared/navbar/navbar";
+import { RouterLink } from '@angular/router';
 import { Footer } from "../../../shared/footer/footer";
+import { Navbar } from "../../../shared/navbar/navbar";
 
 @Component({
   selector: 'app-initial',
-  imports: [Navbar, Footer],
+  imports: [Navbar, Footer, RouterLink],
   templateUrl: './initial.html',
   styleUrl: './initial.scss'
 })
