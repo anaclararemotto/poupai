18eca2fecf183a0787e7b9e290826948bd08c7e4
Clara <clararemotto@gmail.com>
2025-07-02 10:22:20 -0300

    feat: create services page

--- src/app/features/services/services/services.ts
+++ src/app/features/services/services/services.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { Navbar } from "../../../shared/navbar/navbar";
+import { Footer } from "../../../shared/footer/footer";
+import { ServicesTop } from "../services-top/services-top";
+import { ServicesBottom } from "../services-bottom/services-bottom";
 
 @Component({
   selector: 'app-services',
-  imports: [],
+  imports: [Navbar, Footer, ServicesTop, ServicesBottom],
   templateUrl: './services.html',
   styleUrl: './services.scss'
 })
